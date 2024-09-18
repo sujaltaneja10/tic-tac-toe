e98df8f1f93aa1c246f2a1aff72478da5808aadb
Sujal Taneja <sujaltaneja10@gmail.com>
2024-09-18 23:34:43 +0530

    recfactored code into factories and modules

--- script.js
+++ script.js
@@ -1,133 +1,98 @@
+// note for self :-
+// change checkWinner() logic
+// created more arrow functions
+// removed 'this' , bcz, it is not used in factories.
+// refactored to use factories and modules efficiently.
+
+const Player = function(name, marker) {
+    return { name, marker };
+}
+
 const GameBoard = (function () {
-        this.arr = [];
-        this.rows = 3;
-        this.cols = 3;
-        this.inputRow = 0;
-        this.inputCol = 0;
-        this.turn = 1;
-        this.player = [
-            {
-                name : 'Player 1',
-                marker : 'X'
-            },
-            {
-                name : 'Player 2',
-                marker : 'O'
-            }
-        ]
-        const inputPlayerInfo = function() {
-            for (let i=0; i<2; i++) {
-                this.player[i].name = prompt(`Enter player name for player '${i + 1}' : `);
-            }
-        }
-        const createBoard = function () {
-            this.arr = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
-        }
-        const printBoard = function () {
-            const newArr = this.arr.map(e => [...e]);
-            console.log(newArr);
-        }
-        const inputPosition = function() {
-            this.inputRow = +prompt(`Player ${this.turn + 1} Enter row number : `) - 1;
-            this.inputCol = +prompt(`Player ${this.turn + 1} Enter column number : `) - 1;
-            if (!validMove()) inputPosition();
-        };
-        const switchPlayerTurn = function() {
-            if (this.turn == 0) this.turn = 1;
-            else this.turn = 0;
-        }
-        const putMarker = function () {
-            this.arr[this.inputRow][this.inputCol] = player[this.turn].marker;
-        }
-        const newGame = function () {
-            createBoard();
-            inputPlayerInfo();
-            while (checkEmptySpaces() && (!checkWinner())) {
-                switchPlayerTurn();
-                newRound();
-            }
-            decision();
-            this.inputRow = 0;
-            this.inputCol = 0;
-            this.turn = 0;
-            ((prompt('Press "YES" for new game : ')).toLowerCase === 'yes') ? newGame() : endGame();
-        }
-        const checkEmptySpaces = function () {
-            for (let i=0; i<rows; i++) {
-                for (let j=0; j<cols; j++) {
-                    if (this.arr[i][j] == '-') return 1;
-                }
-            }
-            return 0;
-        }
-        const decision = function() {
-            if (checkWinner() == 1 || checkWinner() == 2) {
-                alert(`Winner is ${this.player[checkWinner()-1].name}`);
-            }
-            else {
-                alert('Math is drawn');
-            }
-        }
-        const validMove = function () {
-            if (this.arr[this.inputRow][this.inputCol] === '-') return 1;
-            else return 0;
-        }
-        const newRound = function () {
-            inputPosition();
-            putMarker();
-            printBoard();
-        }
-        const endGame = function() {
-            console.log('Thanks for playing');
-        }
-        const checkWinner = function () {
-            // return 1 if winner found, else zero.
-            
-            // check for rows.
-            for (let i=0; i<rows; i++) {
-                let sum = '';
-                for (let j=0; j<cols; j++) {
-                    sum += arr[i][j];
-                }
-                if (sum == 'XXX') return 1;
-                else if (sum === 'OOO') return 2;
-                else continue;
-            }
+    let arr = [];
+    const rows = 3;
+    const cols = 3;
 
-            // check for columns
-            for (let i=0; i<cols; i++) {
-                let sum = '';
-                for (let j=0; j<rows; j++) {
-                    sum += arr[j][i];
-                }
-                if (sum == 'XXX') return 1;
-                else if (sum === 'OOO') return 2;
-                else continue;
-            }
+    const createBoard = () => arr = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
 
-            // check for diagonals.
-            let sum = '';
-            for (let i=0; i<rows; i++) {
-                for (let j=0; j<cols; j++) {
-                    if (i === j) sum += arr[i][j];
-                }
-            }
-            if (sum == 'XXX') return 1;
-            if (sum === 'OOO') return 2;
+    const printBoard = () => console.log(arr.map(e => [...e]));
 
-            // check for other diagonal.
-            sum = '';
-            for (let i=0; i<rows; i++) {
-                for (let j=0; j<cols; j++) {
-                    if (i + j === 4) sum += arr[i][j];
-                }
-            }
-            if (sum == 'XXX') return 1;
-            if (sum === 'OOO') return 2;
+    const checkEmptySpaces = () => arr.flat().includes('-');
 
-            // else.
-            return 0;
+    const putMarker = (row, col, marker) => {
+        if (validMove(row, col)) arr[row][col] = marker;
+    };
 
+    const validMove = (row, col) => (arr[row][col] === '-');
+
+    const checkWinner = () => {
+        for (let i = 0; i < rows; i++) {
+            if (arr[i][0] === arr[i][1] && arr[i][1] === arr[i][2] && arr[i][0] !== '-') return arr[i][0] === 'X' ? 1 : 2;
         }
-        newGame();
+        for (let i = 0; i < cols; i++) {
+            if (arr[0][i] === arr[1][i] && arr[1][i] === arr[2][i] && arr[0][i] !== '-') return arr[0][i] === 'X' ? 1 : 2;
+        }
+        if (arr[0][0] === arr[1][1] && arr[1][1] === arr[2][2] && arr[0][0] !== '-') return arr[0][0] === 'X' ? 1 : 2;
+        if (arr[0][2] === arr[1][1] && arr[1][1] === arr[2][0] && arr[0][2] !== '-') return arr[0][2] === 'X' ? 1 : 2;
+        return 0;
+    }
+
+    return { createBoard, printBoard, checkEmptySpaces, putMarker, validMove, checkWinner };
 })()
+
+const GameController = function() {
+    let inputRow = 0;
+    let inputCol = 0;
+    let turn = 1;
+
+    const inputPosition = () => {
+        inputRow = +prompt(`Player ${turn + 1} : ${currentPlayer().name} : Enter row number (1-3): `) - 1;
+        inputCol = +prompt(`Player ${turn + 1} : ${currentPlayer().name} : Enter column number (1-3): `) - 1;
+        if (!GameBoard.validMove(inputRow, inputCol)) inputPosition();
+    };
+
+    const currentPlayer = () => turn === 0 ? player1 : player2;
+
+    const switchPlayerTurn = () => turn === 0 ? turn = 1 : turn = 0;
+
+    const endGame = () => console.log('Thanks for playing');
+
+    const decision = () => {
+        if (GameBoard.checkWinner() === 0) {
+            alert('Match is drawn');
+        }
+        else {
+            alert(`Winner is ${currentPlayer().name}`);
+        }
+    }
+
+    const askForNewGame = () => {
+        inputRow = 0;
+        inputCol = 0;
+        turn = 0;
+        ((prompt('Press "YES" for new game : ')).toLowerCase() === 'yes') ? newGame() : endGame();
+    }
+
+    const newRound = () => {
+        inputPosition();
+        GameBoard.putMarker(inputRow, inputCol, currentPlayer().marker);
+        GameBoard.printBoard();
+    }
+
+    const newGame = () => {
+        GameBoard.createBoard();
+        while (GameBoard.checkEmptySpaces() && (!GameBoard.checkWinner())) {
+            switchPlayerTurn();
+            newRound();
+        }
+        decision();
+        askForNewGame();
+    }
+    return { newGame };
+}
+
+const player1 = Player(prompt("Enter name of player 1, 'X'"), 'X');
+const player2 = Player(prompt("Enter name of player 2, 'O'"), 'O');
+
+const game = GameController();
+game.newGame();
